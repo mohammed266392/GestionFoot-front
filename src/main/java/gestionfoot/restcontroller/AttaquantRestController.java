32be9c5324bdb9d1cfe8c43f5fe30dfe1e571810
BENLEMOUY <amelia.benlemouy@gmail.com>
2022-08-19 11:29:48 +0200

    Init objets avec repositories

--- src/main/java/gestionfoot/restcontroller/AttaquantRestController.java
+++ src/main/java/gestionfoot/restcontroller/AttaquantRestController.java
@@ -4,28 +4,31 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import gestionFoot.model.Attaquant;
-import gestionFoot.repository.AttaquantRepository;
+import gestionFoot.service.AttaquantService;
 
 
 @RestController
 @RequestMapping("/api/attaquant")
 public class AttaquantRestController {
 	
+	
 	@Autowired
-	AttaquantRepository attaquantRepo;
+	private AttaquantService attaquantService;
+	
 	
 	@GetMapping("")
-	public String demo() {
-		return "hello toi ? ca va ";
+	public List<Attaquant> getAll() {
+		return attaquantService.getAll();
 	}
 	
-//	@GetMapping("")
-//	public List<Attaquant> getAll() {
-//		return attaquantRepo.findAll();
-//	}
+	@PostMapping("")
+	public Attaquant create(Attaquant attaquant) {
+		return attaquantService.create(attaquant);
+	}
 	
 }
