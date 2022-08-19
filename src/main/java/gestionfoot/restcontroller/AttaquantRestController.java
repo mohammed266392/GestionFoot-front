97c843f313a5f518799ee16fa6e7cb165ad5bcb5
BENLEMOUY <amelia.benlemouy@gmail.com>
2022-08-19 12:12:30 +0200

    Update service web Attaquant

--- src/main/java/gestionfoot/restcontroller/AttaquantRestController.java
+++ src/main/java/gestionfoot/restcontroller/AttaquantRestController.java
@@ -1,13 +1,24 @@
 package gestionfoot.restcontroller;
 
+import java.lang.reflect.Field;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.util.ReflectionUtils;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
+import gestionFoot.exception.AttaquantException;
 import gestionFoot.model.Attaquant;
 import gestionFoot.service.AttaquantService;
 
@@ -27,8 +38,44 @@ public class AttaquantRestController {
 	}
 	
 	@PostMapping("")
-	public Attaquant create(Attaquant attaquant) {
+	public Attaquant create(@RequestBody Attaquant attaquant) {
 		return attaquantService.create(attaquant);
+	}
+	
+	@PutMapping("/{id}")
+	public Attaquant update(@RequestBody Attaquant attaquant, @PathVariable Integer id) {
+		try {
+			Attaquant attaquantEnBase = attaquantService.getById(id);
+			if (attaquantEnBase != null) {
+				attaquant.setId(id);
+			}
+		} catch (AttaquantException ex) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+		}
+		return attaquantService.update(attaquant);
+	}
+	
+	@PatchMapping("/{id}")
+    public Attaquant partalUpdateAttaquant(@RequestBody Map<String, Object> fields,@PathVariable Integer id ) {
+        try {
+            Attaquant fourni = attaquantService.getById(id);
+            fields.forEach((k, v) -> {
+
+                Field field = ReflectionUtils.findField(Attaquant.class, k);
+                ReflectionUtils.makeAccessible(field);
+                ReflectionUtils.setField(field, fourni, v); // ne fonctionne que pour les types standards
+
+            });
+            return attaquantService.update(fourni);
+        }catch(RuntimeException ex) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+
+    }
+	
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable Integer id) {
+		attaquantService.deleteById(id);
 	}
 	
 }
